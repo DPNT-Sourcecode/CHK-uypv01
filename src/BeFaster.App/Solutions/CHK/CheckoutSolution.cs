6fa20df174c87c10e4283b328a0ada7dc85c65fa
sbx_user1051 <sbx_user1051@169.254.6.13>
2024-04-02 21:18:56 +0000

    Tue Apr 02 21:18:56 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,4 +1,5 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions.CHK
 {
@@ -6,7 +7,45 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string skus)
         {
-            throw new SolutionNotImplementedException();
+            int total = 0;
+            int totalAs = 0;
+            int totalBs = 0;
+
+            Dictionary<char, int> priceTable = new Dictionary<char, int>()
+            {
+                {'A', 50 },
+                {'B', 30 },
+                {'C', 20 },
+                {'D', 15 }
+            };
+
+            List<char> specialOfferItems = new List<char>();
+            specialOfferItems.Add('A');
+            specialOfferItems.Add('B');
+
+            foreach (char item in skus)
+            {
+                if (!priceTable.ContainsKey(item))
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (specialOfferItems.Contains(item))
+                    {
+                        totalAs += item == 'A' ? 1 : 0;
+                        totalBs += item == 'B' ? 1 : 0;
+                    }
+                    else
+                    {
+                        total += priceTable[item];
+                    }
+
+                }
+            }
+
+            return total;
         }
     }
 }
+
