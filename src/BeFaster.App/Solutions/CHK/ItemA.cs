82f55ee0ba01b9bc4f9c065b9e9967bc73779bd1
sbx_user1051 <sbx_user1051@169.254.47.37>
2024-04-03 12:18:25 +0000

    Wed Apr 03 12:18:25 UTC 2024

---
+++ src/BeFaster.App/Solutions/CHK/ItemA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public class ItemA : IItem
+    {
+        private const char itemCode = 'A';
+
+        private const int itemPrice = 50;
+
+        public char ItemCode { get { return itemCode; } }
+
+        public int ItemPrice { get { return itemPrice; } }
+
+        public List<Dictionary<int, int>> SpecialOffers { get; } = new List<Dictionary<int, int>>();
+    }
+}
