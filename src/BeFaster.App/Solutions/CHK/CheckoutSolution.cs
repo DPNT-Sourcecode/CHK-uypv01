29d9c21566d9c9698cc55aee01e0611fb6247a9b
sbx_user1051 <sbx_user1051@169.254.47.37>
2024-04-03 13:06:26 +0000

    Wed Apr 03 13:06:26 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -8,28 +8,38 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string skus)
         {
-            int total = 0;
-            int totalAs = 0;
-            int totalBs = 0;
-
             ItemA itemA = new ItemA();
             ItemB itemB = new ItemB();
             ItemC itemC = new ItemC();
             ItemD itemD = new ItemD();
             ItemE itemE = new ItemE();
 
+            int total = 0;
+            int totalAs = 0;
+            int totalBs = 0;
+
+            List<IItem> itemList = new List<IItem>
+            {
+                itemA,
+                itemB,
+                itemC,
+                itemD,
+                itemE
+            };
+
+            Dictionary<char, int> priceTable = new Dictionary<char, int>();
+            foreach (IItem item in itemList)
+            {
+                priceTable.Add(item.ItemCode, item.ItemPrice);
+            }
+
             itemA.SpecialOffers.Add(3, 130);
             itemA.SpecialOffers.Add(5, 200);
 
             itemB.SpecialOffers.Add(2, 45);
 
-            itemE.
+            itemE.SpecialOffers.Add(2, 30);
 
-            itemA.SpecialOffers.Add(new Dictionary<int, int>(3, 130));
-
-            Dictionary<char, Tuple<int, int>> specialOffers = new Dictionary<char, Tuple<int, int>>();
-            specialOffers.Add(itemA, new Tuple<int, int>(3, 130));
-            specialOffers.Add(itemB, new Tuple<int, int>(2, 45));
 
             foreach (char item in skus)
             {
@@ -41,25 +51,39 @@ namespace BeFaster.App.Solutions.CHK
                 {
                     if (priceTable.ContainsKey(item))
                     {
-                        if (item == itemA || item == itemB)
+                        if (item == itemA.ItemCode|| item == itemB.ItemCode)
                         {
-                            totalAs += item == itemA ? 1 : 0;
-                            totalBs += item == itemB ? 1 : 0;
+                            totalAs += item == itemA.ItemCode ? 1 : 0;
+                            totalBs += item == itemB.ItemCode ? 1 : 0;
                         }
                         else
                         {
                             total += priceTable[item];
                         }
-                        
                     }
-
                 }
             }
 
             if (totalAs != 0)
             {
-                int outOfOfferA = totalAs % specialOffers[itemA].Item1;
-                int offerATotal = totalAs / specialOffers[itemA].Item1 * specialOffers[itemA].Item2;
+
+                int outOfOfferA = 0;
+                int offerAtotal = 0;
+
+                //I don't really like using this method of hardcoding numbers, 
+                //but I would need more time to think about how best to do this and would normally refactor later
+                if (totalAs % 3 == 0 && totalAs % 5 == 0)
+                {
+                    offerAtotal += totalAs / 5 * itemA.SpecialOffers[5];
+                }
+                else 
+                {
+                    foreach (var offer in itemA.SpecialOffers)
+                    {
+
+                    }
+                }
+
 
                 if (outOfOfferA == 0)
                 {
@@ -90,3 +114,4 @@ namespace BeFaster.App.Solutions.CHK
         }
     }
 }
+
