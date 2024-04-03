a02280a8248dca1b7521292e1f9558d04f584978
sbx_user1051 <sbx_user1051@169.254.47.37>
2024-04-03 12:09:25 +0000

    Wed Apr 03 12:09:25 UTC 2024

--- src/BeFaster.App/Solutions/CHK/IItem.cs
+++ src/BeFaster.App/Solutions/CHK/IItem.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Solutions.CHK
 {
-    class IItem
+    /// <summary>
+    /// Interface for the properties of an item.
+    /// </summary>
+    public interface IItem
     {
+        /// <summary>
+        /// Item code or SKU.
+        /// </summary>
+        char ItemCode { get; }
+
+        /// <summary>
+        /// Item price.
+        /// </summary>
+        int ItemPrice { get; }
+
+        /// <summary>
+        /// Special offers that apply to the item.
+        /// </summary>
+        List<Dictionary<int, int>> SpecialOffers { get; }
     }
 }
+
