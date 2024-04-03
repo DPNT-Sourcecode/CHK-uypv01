1d3dad21cfcd21ba5c11678732978555a712f03c
sbx_user1051 <sbx_user1051@169.254.47.37>
2024-04-03 19:22:43 +0000

    Wed Apr 03 19:22:43 UTC 2024

--- src/BeFaster.App.Tests/Solutions/CHK/Test.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/Test.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BeFaster.App.Solutions.CHK;
 using NUnit.Framework;
-using BeFaster.App;
 
 namespace BeFaster.App.Tests.Solutions.CHK
 {
-    [TextFixture]
-
+    [TestFixture]
     public class Test
     {
+        List<string> testStr = new List<string>()
+        {
+            { "A" },
+            { "B" }
+        };
+
+        [Test]
+        public void test1()
+        {
+            foreach (var str in testStr)
+            {
+                CheckoutSolution.ComputePrice(str);
+            }
+        }
 
     }
 }
+
 
 
